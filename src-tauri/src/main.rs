2ae297570d4c764a7ca8dc8c43c2d7c778e44921
Daniel Voigt <d.voigt1993@gmail.com>
2024-01-30 15:02:38 +0100

    Try a state management solution with global app state using zustand

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,54 +1,83 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::{
     str::FromStr,
     sync::{Arc, Mutex},
 };
 use tauri::{Manager, State};
 
-#[derive(Clone, serde::Serialize)]
+#[derive(Clone, Serialize)]
 struct StartTimerEventPayload {
     duration: i32,
 }
 
-#[derive(Clone, serde::Serialize)]
+#[derive(Clone, Serialize)]
 struct SetActiveTaskPayload {
     task: String,
 }
 
+#[derive(Copy, Clone, Serialize, Deserialize, Debug)]
 enum WorkPeriod {
     Work,
     SmallBreak,
     BigBreak,
 }
 
-#[derive(Clone, Serialize)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 struct Task {
+    pomodoros: u32,
     name: String,
     done: bool,
 }
 
-struct TaskList {
-    tasks: Mutex<Vec<Arc<Task>>>,
-}
+type TaskItem = Arc<Mutex<Task>>;
+type TaskList = Mutex<Vec<TaskItem>>;
 
-struct TaskLog {
-    period: i32,
-    task: Arc<Task>,
+struct PopupState {
+    popup: Mutex<Option<tauri::Window>>,
 }
 
 struct AppState {
     task_list: TaskList,
-    period_history: Mutex<Vec<TaskLog>>,
-    active_task: Mutex<Option<Arc<Task>>>,
+    active_task: Mutex<Option<TaskItem>>,
     current_period: Mutex<WorkPeriod>,
-    popup: Mutex<Option<tauri::Window>>,
+}
+
+#[derive(Serialize)]
+struct AppStateFrontend {
+    #[serde(rename = "taskList")]
+    task_list: Vec<Task>,
+    #[serde(rename = "activeTask")]
+    active_task: Option<Task>,
+    #[serde(rename = "currentPeriod")]
+    current_period: WorkPeriod,
+}
+
+#[tauri::command]
+fn get_state(state: State<AppState>) -> AppStateFrontend {
+    let tasklist = state.task_list.lock().unwrap();
+    let tasks_fr: Vec<Task> = tasklist
+        .iter()
+        .map(|f| f.as_ref().lock().unwrap().clone())
+        .collect();
+
+    let active_task = state.active_task.lock().unwrap();
+    let active_task_fr = match active_task.as_ref() {
+        Some(task) => Some(task.lock().unwrap().clone()),
+        None => None,
+    };
+
+    AppStateFrontend {
+        task_list: tasks_fr,
+        active_task: active_task_fr,
+        current_period: *state.current_period.lock().unwrap(),
+    }
 }
 
 #[tauri::command]
 fn start_timer(handle: tauri::AppHandle, duration: i32, state: State<AppState>) {
-    let tasklist = state.task_list.tasks.lock().unwrap();
+    let tasklist = state.task_list.lock().unwrap();
     let mut period = state.current_period.lock().unwrap();
     *period = WorkPeriod::Work;
     handle.emit_all("start-timer", StartTimerEventPayload { duration: 25 });
@@ -59,14 +88,62 @@ fn start_timer(handle: tauri::AppHandle, duration: i32, state: State<AppState>) 
         handle.emit_all(
             "set-active-task",
             SetActiveTaskPayload {
-                task: tasklist_first_task.name.clone(),
+                task: tasklist_first_task.lock().unwrap().name.clone(),
             },
         );
     }
 }
 
+#[derive(Deserialize, Serialize, Clone, Debug)]
+#[serde(untagged)]
+enum StatePayload {
+    Tasks(Vec<Task>),
+    Task(Task),
+    WorkPeriod(WorkPeriod),
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+struct StateSynchEvent {
+    key: String,
+    value: StatePayload,
+}
+
 #[tauri::command]
-async fn close_popup(state: State<'_, AppState>) -> Result<(), ()> {
+fn mutate_state(
+    key: String,
+    value: StatePayload,
+    state: State<'_, AppState>,
+    handle: tauri::AppHandle,
+) -> Result<(), ()> {
+    eprint!("Got Command! {value:?}");
+    match key.as_str() {
+        "tasks" => {
+            if let StatePayload::Tasks(t) = value {
+                let mut tasks = state.task_list.lock().unwrap();
+                *tasks = t.into_iter().map(|ta| Arc::new(Mutex::new(ta))).collect();
+
+                let cloned = tasks
+                    .iter()
+                    .map(|f| f.as_ref().lock().unwrap().clone())
+                    .collect();
+
+                handle.emit_all(
+                    "synch_state",
+                    StateSynchEvent {
+                        key: String::from("tasks"),
+                        value: StatePayload::Tasks(cloned),
+                    },
+                );
+            }
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
+
+#[tauri::command]
+async fn close_popup(state: State<'_, PopupState>) -> Result<(), ()> {
     let mut popup_m = state.popup.lock().unwrap();
     if let Some(popup) = popup_m.as_ref() {
         let _ = popup.close();
@@ -76,7 +153,7 @@ async fn close_popup(state: State<'_, AppState>) -> Result<(), ()> {
 }
 
 #[tauri::command]
-async fn open_popup(handle: tauri::AppHandle, state: State<'_, AppState>) -> Result<(), ()> {
+async fn open_popup(handle: tauri::AppHandle, state: State<'_, PopupState>) -> Result<(), ()> {
     let mut popup_m = state.popup.lock().unwrap();
     if let Some(popup) = popup_m.as_ref() {
         popup.show().unwrap();
@@ -101,14 +178,18 @@ async fn open_popup(handle: tauri::AppHandle, state: State<'_, AppState>) -> Res
 
 #[tauri::command]
 fn add_task(name: &str, state: State<AppState>) -> Vec<Task> {
-    let mut tasklist = state.task_list.tasks.lock().unwrap();
-    tasklist.push(Arc::new(Task {
+    let mut tasklist = state.task_list.lock().unwrap();
+    tasklist.push(Arc::new(Mutex::new(Task {
+        pomodoros: 0,
         name: String::from_str(name).unwrap(),
         done: false,
-    }));
+    })));
 
-    let cloned = tasklist.iter().map(|f| f.as_ref().clone()).collect();
-    return cloned;
+    let cloned = tasklist
+        .iter()
+        .map(|f| f.as_ref().lock().unwrap().clone())
+        .collect();
+    cloned
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -119,21 +200,23 @@ fn greet(name: &str) -> String {
 
 fn main() {
     let app_state = AppState {
-        task_list: TaskList {
-            tasks: Mutex::new(Vec::new()),
-        },
+        task_list: Mutex::new(Vec::new()),
         active_task: Mutex::new(None),
-        period_history: Mutex::new(Vec::new()),
         current_period: Mutex::new(WorkPeriod::Work),
+    };
+    let popup_state = PopupState {
         popup: Mutex::new(None),
     };
     tauri::Builder::default()
         .manage(app_state)
+        .manage(popup_state)
         .invoke_handler(tauri::generate_handler![
             greet,
             open_popup,
             start_timer,
             add_task,
+            get_state,
+            mutate_state,
             close_popup
         ])
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,112 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import {
-    BotIcon,
-    MenuIcon,
-    PlayIcon,
-    PlusIcon,
-    ShieldCloseIcon,
+  BotIcon,
+  MenuIcon,
+  PlayIcon,
+  PlusIcon,
+  ShieldCloseIcon,
 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import { Checkbox } from "./components/ui/checkbox";
 import { Input } from "./components/ui/input";
-
-type Task = {
-    name: string;
-    done: boolean;
-};
+import {
+  subscribeAppState,
+  useAppState,
+  useSynchAppState,
+} from "./lib/app-state";
 
 function App() {
-    const [taskInputText, setTaskInputText] = useState("");
-    const [tasks, setTasks] = useState<Array<Task>>([]);
+  const tasks = useAppState((state) => state.tasks);
+  const addTaskToState = useAppState((state) => state.addTask);
+  useSynchAppState();
+  const [taskInputText, setTaskInputText] = useState("");
 
-    const closePopup = async () => {
-        await invoke("close_popup", {});
+  const closePopup = async () => {
+    await invoke("close_popup", {});
+  };
+
+  const showPopup = async () => {
+    await invoke("open_popup", {});
+  };
+
+  const startTimer = async () => {
+    await invoke("start_timer", { duration: 25 });
+  };
+
+  useEffect(() => {
+    const unsub = subscribeAppState();
+    return () => {
+      unsub.then((us) => us());
     };
+  }, []);
 
-    const showPopup = async () => {
-        await invoke("open_popup", {});
-    };
+  const addTask = async () => {
+    const name = taskInputText;
+    addTaskToState({ pomodoros: 0, name, done: false });
+    setTaskInputText("");
+  };
 
-    const startTimer = async () => {
-        await invoke("start_timer", { duration: 25 });
-    };
-
-    const addTask = async () => {
-        const name = taskInputText;
-        const tasks = await invoke("add_task", { name });
-        setTasks(tasks as Task[]);
-        setTaskInputText("");
-    };
-
-    return (
-        <div className="h-screen">
-            <div className="bg-primary text-primary-foreground h-44 flex flex-col rounded-b-3xl drop-shadow-md">
-                <div className="flex justify-between px-8 py-2 ">
-                    <h1 className="text-4xl font-bold">YiPomo</h1>
-                    <div>
-                        <MenuIcon className="h-4 w-4" />
-                    </div>
-                </div>
-                <div className="flex px-8 items-end h-full py-4 gap-4">
-                    <h1 className="text-8xl font-bold leading-none">25</h1>
-                    <div className="flex gap-2 w-full flex-col pb-4">
-                        <p>Figma MVP Design</p>
-                        <div className="rounded-lg bg-primary-foreground w-full relative h-3">
-                            <div className="rounded-lg bg-red-400 w-1/3 absolute left-[2px] top-[2px] h-2 drop-shadow" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="px-8 py-8 flex flex-col gap-4 max-h-[60vh] overflow-y-scroll">
-                <h2 className="text-2xl font-bold">Tasks</h2>
-                <ul className="flex flex-col gap-3">
-                    {tasks.map((task) => (
-                        <li className="flex gap-2 items-center" key={task.name}>
-                            <Checkbox id="terms2" checked={task.done} />
-                            <span className="font-medium">{task.name}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="border-b border-muted-foreground h-1 w-full px-8" />
-            <div className="flex gap-2 items-center px-8">
-                <Input
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            addTask();
-                        }
-                    }}
-                    className="w-full"
-                    type="text"
-                    placeholder="New Task"
-                    value={taskInputText}
-                    onChange={(e) => setTaskInputText(e.target.value)}
-                />
-                <Button onClick={() => addTask()}>
-                    <PlusIcon className="w-4 h-4" />
-                </Button>
-                <Button onClick={() => showPopup()}>
-                    <BotIcon className="w-4 h-4" />
-                </Button>
-                <Button onClick={() => closePopup()}>
-                    <ShieldCloseIcon className="w-4 h-4" />
-                </Button>
-                <Button onClick={() => startTimer()}>
-                    <PlayIcon className="w-4 h-4" />
-                </Button>
-            </div>
+  return (
+    <div className="h-screen">
+      <div className="bg-primary text-primary-foreground h-44 flex flex-col rounded-b-3xl drop-shadow-md">
+        <div className="flex justify-between px-8 py-2 ">
+          <h1 className="text-4xl font-bold">YiPomo</h1>
+          <div>
+            <MenuIcon className="h-4 w-4" />
+          </div>
         </div>
-    );
+        <div className="flex px-8 items-end h-full py-4 gap-4">
+          <h1 className="text-8xl font-bold leading-none">25</h1>
+          <div className="flex gap-2 w-full flex-col pb-4">
+            <p>Figma MVP Design</p>
+            <div className="rounded-lg bg-primary-foreground w-full relative h-3">
+              <div className="rounded-lg bg-red-400 w-1/3 absolute left-[2px] top-[2px] h-2 drop-shadow" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-8 py-8 flex flex-col gap-4 max-h-[60vh] overflow-y-scroll">
+        <h2 className="text-2xl font-bold">Tasks</h2>
+        <ul className="flex flex-col gap-3">
+          {tasks.map((task) => (
+            <li className="flex gap-2 items-center" key={task.name}>
+              <Checkbox id="terms2" checked={task.done} />
+              <span className="font-medium">{task.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="border-b border-muted-foreground h-1 w-full px-8" />
+      <div className="flex gap-2 items-center px-8">
+        <Input
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              addTask();
+            }
+          }}
+          className="w-full"
+          type="text"
+          placeholder="New Task"
+          value={taskInputText}
+          onChange={(e) => setTaskInputText(e.target.value)}
+        />
+        <Button onClick={() => addTask()}>
+          <PlusIcon className="w-4 h-4" />
+        </Button>
+        <Button onClick={() => showPopup()}>
+          <BotIcon className="w-4 h-4" />
+        </Button>
+        <Button onClick={() => closePopup()}>
+          <ShieldCloseIcon className="w-4 h-4" />
+        </Button>
+        <Button onClick={() => startTimer()}>
+          <PlayIcon className="w-4 h-4" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
